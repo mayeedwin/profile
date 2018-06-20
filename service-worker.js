2e85b22c9342499b9770f58da6d3be061282c85d
Maye Edwin <mayedwine1@gmail.com>
2018-06-20 10:16:37 +0300

    clean-errors
    clean-errors

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
-      // after a service worker is installed and the user navigates to a different page or refreshes,the service worker will begin to receive fetch events
-          
-      self.addEventListener('fetch', function(event) {
+     self.addEventListener('fetch', function(event) {
         event.respondWith(caches.open('cache').then(function(cache) {
           return cache.match(event.request).then(function(response) {
             console.log("cache request: " + event.request.url);
@@ -25,12 +23,12 @@
                       '/index.html', //default
                       '/index.html?homescreen=1', //default
                       '/?homescreen=1', //default
-                      '/assets/main.css', // configure as by your site ; just an example
-                      '/icons/pwafire128white.png', // add more icons path below as in this line. Check out the icons path in the icons folder.
+                      'assets/main.css', // configure as by your site ; just an example
+                      'icons/pwafire128white.png', // add more icons path below as in this line. Check out the icons path in the icons folder.
                       
-  // Do not replace/delete/edit the sw.js/ and manifest.js paths below
-                      '/sw.js/',
-                      '/manifest.js',
+  // Do not replace/delete/edit the service-worker.js/ and manifest.js paths below
+                      'service-worker.js',
+                      'manifest.js',
   //These are links to the extenal social media buttons that should be cached; we have used twitter's as an example
                       'https://platform.twitter.com/widgets.js',       
                       ]);
@@ -42,3 +40,19 @@
                   });
                   }));
                   });
+
+                  self.addEventListener('message', (event) => {
+                    if (!event.data){
+                      return;
+                    }
+                  
+                    switch (event.data) {
+                      case 'skipWaiting':
+                        self.skipWaiting();
+                        break;
+                      default:
+                        // NOOP
+                        break;
+                    }
+                  });
+                  
